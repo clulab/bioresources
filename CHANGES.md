b6619f2e5bc3eff5be725b756ad80e4a26280f95
Mihai Surdeanu <surdeanu@gmail.com>
2016-02-08 09:58:09 -0700

    Added the ner kbs

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,4 @@
 #Changes
 =======
++ **1.1.0** - Added KBs in the format expected by the BioNLPProcessor NER.
 + **1.0.0** - Initial release. Copied KB resources from Reach 1.2.3-SNAPSHOT. These are used both by reach and processors (for BioNLPProcessor).
---
+++ ner_kb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# Converts the Reach KBs into the format expected by BioNLPProcessor
+# Re-run this scripts whenever a Reach KB changes
+# To avoid rugenerating *all* KBs edit the ner_kb.config file and keep only the modified KBs
+#
+
+cd ../processors
+sbt 'run-main edu.arizona.sista.processors.bionlp.ner.KBGenerator ../bioresources/ner_kb.config ../bioresources/src/main/resources/org/clulab/reach/kb/ ../bioresources/src/main/resources/org/clulab/reach/kb/ner'
+cd ../bioresources
+
+
