76b8dc102faace65e404423da87c7e600ddc9bac
Mihai Surdeanu <surdeanu@gmail.com>
2017-11-10 21:53:51 -0700

    Added serialized LexiconNER model.

--- ner_kb.sh
+++ ner_kb.sh
@@ -2,12 +2,18 @@
 
 #
 # Converts the Reach KBs into the format expected by BioNLPProcessor
-# Re-run this scripts whenever a Reach KB changes
+# Re-run this script whenever a Reach KB changes
 # To avoid rugenerating *all* KBs edit the ner_kb.config file and keep only the modified KBs
 #
 
 cd ../processors
+
+# generete the NER KBs here
 sbt 'run-main org.clulab.processors.bionlp.ner.KBGenerator ../bioresources/ner_kb.config ../bioresources/src/main/resources/org/clulab/reach/kb/ ../bioresources/src/main/resources/org/clulab/reach/kb/ner'
+
+# generate the serialized LexiconNER model now
+sbt 'run-main org.clulab.processors.bionlp.ner.KBLoader ../bioresources/src/main/resources/org/clulab/reach/kb/ner/model.ser.gz'
+
 cd ../bioresources
 
 
