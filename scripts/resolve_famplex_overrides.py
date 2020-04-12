76c9450a6b0efac4f18b548dc4784450ec881057
Ben Gyori <ben.gyori@gmail.com>
2020-04-12 00:12:10 -0400

    Replace HMDB overrides with other namespaces

--- scripts/resolve_famplex_overrides.py
+++ scripts/resolve_famplex_overrides.py
@@ -13,7 +13,7 @@ resolve it to keep a single valid override.
 import os
 import requests
 
-base_url = 'https://raw.githubusercontent.com/bgyori/famplex/91a56c4b01c470ce83c7dd7bb53f418cbf294767/'
+base_url = 'https://raw.githubusercontent.com/bgyori/famplex/reach_export/'
 
 
 def get_non_fplx_overrides():
