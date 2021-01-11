46cb366d86a6447af2411958b44e30e28fd60600
Ben Gyori <ben.gyori@gmail.com>
2021-01-11 13:10:59 -0500

    Add extra synonyms for CD4/CD8 T cells

--- scripts/update_cell_ontology.py
+++ scripts/update_cell_ontology.py
@@ -6,6 +6,27 @@ import pickle
 import obonet
 
 
+extra_synonyms = {
+    # CD4-positive, alpha-beta T cell
+    # This is actually a slightly narrower category but is a "close enough"
+    # match for these synonyms. We then override these in the overrides
+    # file to exact matches in MeSH.
+    'CL:CL:0000624': [
+        'CD4+', 'CD4 +', 'CD4+ T cell',
+        'CD4+ T cells', 'CD4 + T cells', 'CD4 þ T cells', 'CD4 ϩ T cells',
+        'CD4 T cells'
+    ],
+    'CL:0000492': [
+        'CD4+ T helper', 'CD4+ T helper cells'
+    ],
+    'CL:0000625': [
+        'CD8+', 'CD8 +', 'CD8+ T cell',
+        'CD8+ T cells', 'CD8 + T cells', 'CD8 þ T cells', 'CD8 ϩ T cells',
+        'CD8 T cells'
+    ],
+}
+
+
 def read_cached_url(url, cache_file):
     if not os.path.exists(cache_file):
         print('Loading %s' % url)
@@ -73,6 +94,10 @@ if __name__ == '__main__':
         # Format of the output defined here
         entries += [(txt, cl_id) for txt in ([name] + synonyms + pluralized)
                     if accept_entry(name, txt, cl_id, data)]
+
+    # Add extra synonyms
+    for cl_id, synonyms in extra_synonyms.items():
+        entries += [(txt, cl_id) for txt in synonyms]
 
     # We sort the entries first by the synonym but in a way that special
     # characters and capitalization is ignored, then sort by ID
